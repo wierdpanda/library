98616490747622addd3bccd34cd749c80cb8b1af
wierdpanda <60147367+wierdpanda@users.noreply.github.com>
2024-04-10 14:46:13 +0200

    set up breeze/sass sorted out vite connections
    created site new.blade.php to test and ensure all tailwind classes working
    can now start on library

---
+++ resources/views/new.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" class="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>new</title>
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+</head>
+<body class= "bg-gray-100 dark:bg-gray-900">
+
+    <h2 class="flex justify-center text-cyan-400">all 4 h2 use text-cyan-400 tailwind class color </h2>
+    
+</body>
+</html>
+
+{{-- class= "bg-gray-100 dark:bg-gray-900"
+
+class="dark" --}}
